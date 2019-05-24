67e5d3c9eed6e1c86cf9c35b89669a0a409acfaa
yushuifeng <yushuifeng@vip.qq.com>
2019-05-24 20:20:06 +0800

    upgrade version check
    Signed-off-by: yushuifeng <yushuifeng@vip.qq.com>

--- packages/oc-cli/src/getAutoUpdate.js
+++ packages/oc-cli/src/getAutoUpdate.js
@@ -8,10 +8,10 @@ const chalk = require('chalk');
 const path = require('path');
 
 module.exports = () => {
-    request({ url: 'http://iuap-design-cdn.oss-cn-beijing.aliyuncs.com/static/uba/ucf-cli-version.json' }, (error, response, body) => {
+    request({ url: 'https://github.com/omni-channel-cloud/occ-web/blob/master/packages/oc-cli/package.json' }, (error, response, body) => {
         let result = JSON.parse(body);
         let version = require('../package.json').version;
-        if(result['occ-cli'] != version){
+        if(result['oc-cli'] != version){
             console.log(chalk.yellow.bold(`New version ${version} -> ${result['oc-cli']}`));
             console.log(chalk.yellow.bold(`npm install oc-cli -g`));
         }
